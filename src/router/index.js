2d67c37fc2ac8ed9ce63218e58aa5c9267109207
NadyaSukhova <suhova1313@gmail.com>
2023-12-14 23:03:19 +0300

    reinstalled gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import CharacterPage from "@/components/CharacterPage.vue";
 import CharacterList from "@/components/CharacterList.vue";
 import EpisodePage from "@/components/EpisodePage.vue";
@@ -38,7 +38,7 @@ const routes = [
   },
 ];
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: routes
 });
 export default router;
